64953e5fb20114fb9d8f583d5f659ea673b7bf6e
djbatt <djbatt@live.com>
2018-06-26 12:13:55 -0400

    html finished, added database config

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var config = {
+      apiKey: "AIzaSyBx0Dm-tDBPq5qXxL0xT7TnE8BKMsI6VAk",
+      authDomain: "traindatabase-7ea50.firebaseapp.com",
+      databaseURL: "https://traindatabase-7ea50.firebaseio.com",
+      projectId: "traindatabase-7ea50",
+      storageBucket: "",
+      messagingSenderId: "142235789754"
+    };
+});
